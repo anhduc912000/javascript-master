a4ded4dccb74f7f4786dd6b838964a3b690617ef
anhduc <anhduc912000@gmail.com>
2018-11-11 13:25:22 +0700

    bind.js

--- bind.js
+++ bind.js
@@ -1,14 +1,26 @@
+// var mouse = {
+// 	name: 'Mickey',
+// 	sayHi: function(){
+// 		console.log('hi, my name is', this.name);
+// 	}
+// }
+
+// var say = mouse.sayHi.bind(mouse);
+
+// var cat = {
+// 	name: 'tom';
+// }
+
+// var say = cat.sayHi.bind(cat);
+//method bind tạo ra một function mới và set thánh biến cat
+
+var btn = document.getElementById('btn');
+
 var mouse = {
-	name: 'Mickey',
-	sayHi: function(){
-		console.log('hi, my name is', this.name);
+	name: ' Mickey',
+	sayHello: function(){
+		alert('My name is' + this.name);
 	}
-}
+};
 
-var say = mouse.sayHi.bind(mouse);
-
-var cat = {
-	name: 'tom'
-}
-
-var say =cat.sayHi.bind(cat);
+btn.addEventListener('click', mouse.sayHello.bind(mouse));
